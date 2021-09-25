f1ca5b3af4cff2509feec1166e940b029d2e5853
shamsmosowi <shams.mosowi@gmail.com>
2021-09-25 19:58:50 +1000

    confirm firestore is initialised

--- src/hooks/prebuild.ts
+++ src/hooks/prebuild.ts
@@ -1,22 +1,48 @@
 import { updateConfig, getProjectId } from "./utils";
 import { db } from "../firebaseConfig";
 import { logError } from "./createRowyApp";
+import inquirer from "inquirer";
+const projectId = getProjectId();
+const publicSettings = {
+  signInOptions: ["google"],
+};
+const settings = {
+  rowyRunBuildStatus: "BUILDING",
+  rowyRunRegion: process.env.GOOGLE_CLOUD_REGION,
+};
+
+const initFirestore = async () => {
+  await db.doc("_rowy_/settings").set(settings, { merge: true });
+  await db.doc("_rowy_/publicSettings").set(publicSettings, { merge: true });
+};
+const confirmFirestoreIsInitialised = async () => {
+  try {
+    await initFirestore();
+  } catch (error) {
+    inquirer.prompt([
+      {
+        type: "confirm",
+        name: "firestore",
+        message:
+          "Firestore is not initialized. please insure it has been initialized before starting",
+      },
+    ]);
+    return await confirmFirestoreIsInitialised();
+  }
+};
+
 async function start() {
   try {
-    const projectId = getProjectId();
     if (!projectId) {
       throw new Error("GOOGLE_CLOUD_PROJECT env variable is not set");
     }
+
+    try {
+      await initFirestore();
+    } catch (error) {
+      await confirmFirestoreIsInitialised();
+    }
     updateConfig("projectId", projectId);
-    const settings = {
-      rowyRunBuildStatus: "BUILDING",
-      rowyRunRegion: process.env.GOOGLE_CLOUD_REGION,
-    };
-    await db.doc("_rowy_/settings").set(settings, { merge: true });
-    const publicSettings = {
-      signInOptions: ["google"],
-    };
-    await db.doc("_rowy_/publicSettings").set(publicSettings, { merge: true });
   } catch (error) {
     console.log(error);
     logError({
